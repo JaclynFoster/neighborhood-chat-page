13e9e771f4d9fb36da55413e6e34d0c1c6f562a6
Jaclyn Foster <jaclynfoster89@gmail.com>
2023-05-30 16:24:49 -0500

    deploying

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const { SERVER_PORT } = process.env
 app.use(express.json())
 app.use(cors())
 
-app.use(express.static(`${__dirname}/../public`))
+app.use(express.static(`${__dirname}/../build`))
 console.log(__dirname)
 
 const {
